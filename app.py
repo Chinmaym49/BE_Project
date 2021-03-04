5e469d5b3cc64579cc831c9daa0a66de5def85fe
Chinmay Mungilwar <chinmaym49@gmail.com>
2021-03-04 17:40:41 +0530

    app.py setup

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,session,request,redirect,render_template,url_for,flash,jsonify
+import mysql.connector
+from datetime import timedelta,datetime
+
+app=Flask(__name__)
+app.secret_key="cD7nTw3jF4cwA1dv"
+app.permanent_session_lifetime=timedelta(days=10)
+
+db=mysql.connector.connect(host="remotemysql.com",user="xjkizVz0D6",password="cQDyeM6Uxx",database="xjkizVz0D6")
+
+@app.route("/")
+def homepage():
+    return "-_-"
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0")
