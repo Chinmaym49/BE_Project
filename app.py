e8d2528ae0cc6b871558a4deb024535442fb3884
Chinmay Mungilwar <chinmaym49@gmail.com>
2021-03-05 21:18:04 +0530

    tagpage optimized

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.secret_key = "cD7nTw3jF4cwA1dv"
 app.permanent_session_lifetime = timedelta(days=10)
 
 conf={"host":"remotemysql.com","user":"xjkizVz0D6","password":"cQDyeM6Uxx","database":"xjkizVz0D6"}
+tags=[]
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -33,11 +34,13 @@ def profile():
 
 @app.route("/tags")
 def tagpage():
-    db=mysql.connector.connect(**conf)
-    cur=db.cursor()
-    cur.execute("select * from Tag")
-    tags=cur.fetchall()
-    cur.close()
+    global tags
+    if len(tags)==0:
+        db=mysql.connector.connect(**conf)
+        cur=db.cursor()
+        cur.execute("select * from Tag")
+        tags=cur.fetchall()
+        cur.close()
     return render_template("tags.html",tags=tags,n=len(tags))
 
 if __name__ == "__main__":
