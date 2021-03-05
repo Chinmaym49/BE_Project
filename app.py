31c9120509d6fe8712ae108e3d4836cc7cf50508
Shailesh Borate <shaileshborate@gmail.com>
2021-03-05 20:38:33 +0530

    app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ db = mysql.connector.connect(
 def home():
     return render_template('index.html')
 
+
 @app.route("/askQuestion", methods=["GET", "POST"])
 def askQuestion():
     if request.method == "POST":
@@ -21,25 +22,27 @@ def askQuestion():
         body = request.form.get("body")
     return render_template('ask.html')
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template('login.html')
+
 
 @app.route("/questions", methods=["GET", "POST"])
 def questions():
     return "Questions"
 
+
 @app.route("/profile", methods=["GET", "POST"])
 def profile():
     return "Profile"
+
 
 @app.route("/tags")
 def tagpage():
     return render_template("tags.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
-
-
-
 
